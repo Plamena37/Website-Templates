baf037de1c393960793b39225ed8197fef8a6dfc
Plamena <yomena37@gmail.com>
2022-06-14 16:46:08 +0300

    Navigation modification

--- Bankist/js/script.js
+++ Bankist/js/script.js
@@ -41,3 +41,85 @@ btnScrollTo.addEventListener('click', function () {
 });
 
 /* PAGE NAVIGATION ******************************************/
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document
+  .querySelector('.nav__links')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Matching strategy
+    if (event.target.classList.contains('nav__link')) {
+      const id = event.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+
+/* TABBED COMPONENT ******************************************/
+// Using event delegation
+tabsContainer.addEventListener('click', function (event) {
+  const clicked = event.target.closest('.operations__tab');
+
+  //Guard clause
+  if (!clicked) {
+    return;
+  }
+
+  // Active tab
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+/* MENU FADE ANIMATION ******************************************/
+const handleHover = function (event) {
+  if (event.target.classList.contains('nav__link')) {
+    const link = event.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing 'argument' into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+/* STICKY NAVIGATION ******************************************/
+// Using Intersection Observer API
+// Getting the navigation height dynamically
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  // default: NULL because we want to observe the entire viewport
+  root: null,
+  threshold: 0,
+  // height of navigation, and MUST be in px
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+/* REVEAL SECTIONS ******************************************/
